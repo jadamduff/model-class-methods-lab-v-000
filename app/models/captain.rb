40cef9e0b78e9c1ce023dc2517eccaeb07b0be19
Adam Duff <j.adam.duff@gmail.com>
2018-09-28 17:02:04 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,6 +2,6 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    Captain.where(joins(:boats).group('boats.captain_id').having('classification.name = Catamaran'))
+    Captain.where(joins(boats: :classifications).where('classification.name = Catamaran'))
   end
 end

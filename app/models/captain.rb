6948c82dfa087a989c04a3efc5452a0c6e0b9bca
Adam Duff <j.adam.duff@gmail.com>
2018-09-28 16:37:37 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,6 +2,6 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    Captain.inlcudes(:boats).select('boats.*')
+    Captain.includes(:boats).select('boats.*')
   end
 end
